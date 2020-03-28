3628623aa19f3386bb2542d287bfd09a415a298f
Tulio Faria <tuliofaria@gmail.com>
2020-03-28 18:10:49 -0300

    initial version react-select and client-side routes.

--- gatsby-node.js
+++ gatsby-node.js
@@ -28,3 +28,14 @@ module.exports.createPages = async ({ actions, graphql }) => {
   })
 
 }
+
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
+  if (page.path.match(/^\/restrito/)) {
+    // page.matchPath is a special key that's used for matching pages
+    // with corresponding routes only on the client.
+    page.matchPath = "/restrito/*"
+    // Update the page.
+    createPage(page)
+  }
+}
--- src/pages/restrito.js
+++ src/pages/restrito.js
@@ -1,12 +1,47 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import firebase from '../components/firebase'
+import { Router } from '@reach/router'
+import Select from 'react-select'
+import states from '../lib/states.json'
+import { Link } from 'gatsby'
+
+const STATES = states.sort((a, b) => a.state > b.state ? 1 : -1).map(state => {
+  return {
+    value: state.abbr,
+    label: state.state + ' (' + state.abbr + ')'
+  }
+})
+
+const Listings = ({ state }, ...props) => {
+  return (
+    <React.Fragment>
+      <h2>Restritos <Link to='/restrito/listings/MG'>MG</Link></h2>
+      <Select options={STATES} defaultValue={state} />
+      <pre>{state} / {JSON.stringify(props, null, 2)}</pre>
+    </React.Fragment>
+  )
+}
 
 const Restrito = () => {
+  const add = () => {
+    firebase
+      .firestore()
+      .collection('listings')
+      .add({
+        status: 'pending'
+      })
+  }
   return(
     <Layout>
       <div className='container mx-auto mt-12'>
         <h1>Restrito</h1>
-        <p>Estamos preparando novidades :)</p>
+        <p onClick={add}>Estamos preparando novidades :)</p>
+        <Router basepath='/restrito'>
+          <Listings path='/listings' />
+          <Listings path='/listings/:state' />
+          <Listings path='/listings/:state/:city' />
+        </Router>
       </div>
     </Layout>
   )
